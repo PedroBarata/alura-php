7fbf760848fcd45930f611f09f1cabf830e8b2f2
PedroBarata <pedrofelipebarata@gmail.com>
2020-11-29 17:18:16 -0300

    feat: teste de erros

---
+++ php-exceptions/erros.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+   var_dump($errno, $errstr, $errfile, $errline);
+   switch ($errno){
+       case E_WARNING:
+           echo 'teste';
+           break;
+       case E_ERROR:
+           echo 'error!';
+           break;
+
+   }
+
+
+   return true;
+});
+
+echo $variavel;
+
+echo $array[12];
